0384abc22eed785345d53c3b4cd2fe07a5df42ef
Alex <sanyatishenko@outlook.com>
2018-11-10 19:36:45 +0000

    Add function Install-Environment. Install soft from file-list with Choco

---
+++ Install-Environment.ps1
@@ -0,0 +1,27 @@
+﻿Function Install-Environment () { 
+
+    Param(
+        [string]$Path
+    )
+    #Choco alredy installed ?
+
+    $s = $env:ProgramData + '\chocolatey\bin\chocolatey.exe'
+
+    if (-not (Get-Item $s -ErrorAction Ignore)) {
+        Write-Host "Choco not installed" -ForegroundColor DarkMagenta
+        Set-ExecutionPolicy Bypass -Scope Process -Force
+        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    } else {
+        Write-Host "Choco alredy installed" -ForegroundColor DarkMagenta
+    }
+
+    [array]$list = Get-Content $Path
+    foreach ($item in $list) {
+        $item = $item.Trim(" ","`t")
+        if ($item -and ($item[0] -ne '#')){
+            Write-Host "------------------------------------" -ForegroundColor DarkMagenta
+            Write-Host "Execute command: choco install -y $item --force" -ForegroundColor DarkMagenta
+            choco install -y $item --force
+        }
+    }
+}
